23eebc593fd09ff7df81881d6c4e75b1c7693ce2
Mattias Ekendahl <mattias@developedbyme.com>
2024-08-23 13:43:37 +0200

    Added: initial version

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+function mt_register_collections_post_type() {
+
+    $args = array(
+        'label'               => 'Collections',
+        'public'              => true,
+        'supports'            => array( 'title', 'editor', 'thumbnail' ),
+        'has_archive'         => true
+    );
+
+    register_post_type( 'collection', $args );
+
+}
+add_action( 'init', 'mt_register_collections_post_type' );
+
+add_action('before_form_on_page_template', function($message_count, $messages) {
+    do_action('before_form_on_page_template_reversed', $messages, $message_count);
+}, 10, 2);
+
+
+add_action('form_on_page_template', 'mt_output_form', 10);
+
+function mt_output_form() {
+    ?>
+        <form method="POST">
+                    <input name="message" />
+                    <input type="submit" />
+                </form>
+    <?php
+}
+
+
+
+
+?>
---
+++ pagetemplate-form-page.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Form page
+*/
+get_header(); ?>
+        This is a form page:
+        <?php
+		if ( have_posts() ) :
+
+			while ( have_posts() ) :
+				the_post();
+				the_title();
+				the_content();
+			endwhile;
+
+		endif;
+		?>
+			<?php
+				if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+					if(isset($_POST['message'])) {
+						add_post_meta(get_the_id(), 'secretMessage', $_POST['message']);
+					}
+					
+				}
+
+				$messages = get_post_meta(get_the_id(), 'secretMessage', false);
+				$messages = apply_filters('secret_messages', $messages, get_the_id(), $messages);
+				
+				if(isset($_GET['showMessage']) && $_GET['showMessage'] == '1') {
+					foreach($messages as $message) {
+						echo('<div>'.$message.'</div>');
+					}
+				}
+			?>
+			<?php do_action('before_form_on_page_template', count($messages), $messages); ?>
+			<?php do_action('form_on_page_template'); ?>
+			<?php do_action('after_form_on_page_template'); ?>
+        <?php
+get_footer();

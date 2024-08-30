8d2610968faab1b34789c8d740fe0f264d0d5a5a
Mattias Ekendahl <mattias@developedbyme.com>
2024-08-30 13:06:41 +0200

    New front page

--- pagetemplate-new-front-page.php
+++ pagetemplate-new-front-page.php
@@ -11,36 +11,15 @@ get_header();
 
 			while ( have_posts() ) :
 				the_post();
+
 				the_title();
+
 				the_content();
+
 			endwhile;
 
 		endif;
-		?>
-        <h2>Senaste kollektioner</h2>
-        <?php
-             add_filter( 'mp_get_latest_collections_query_args', function($args) {
-                
-                $args['posts_per_page'] = 4;
-
-                return $args
-             } );
-            $query = apply_filters("mp_get_latest_collections", null);
-
-            if($query) {
-                while($query->have_posts()) {
-                    $query->the_post();
-    
-                    ?><a href="<?php echo(get_permalink(get_the_ID())); ?>">
-                    <?php
-                    the_title();
-                    ?>
-                    </a>
-                    <?php
-                }
-    
-                wp_reset_postdata();
-            }
+		
             
         ?>
         <?php

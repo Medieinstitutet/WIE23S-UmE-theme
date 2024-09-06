ce0902c5e854fea7326b69d7521d0096c651f990
Mattias Ekendahl <mattias@developedbyme.com>
2024-09-06 09:24:10 +0200

    Correct form post

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 
 
-add_action('before_form_on_page_template', function($message_count, $messages) {
-    do_action('before_form_on_page_template_reversed', $messages, $message_count);
-}, 10, 2);
-
-
 add_action('form_on_page_template', 'mt_output_form', 10);
 
 function mt_output_form() {
     ?>
         <form method="POST">
-                    <input name="message" />
+                    <input name="title" />
+                    <textarea name="content">
+
+</textarea>
+<select name="selectedProducts[]" multiple>
+    <option value="0">Test</option>
+    <option value="1">Test</option>
+</select>
                     <input type="submit" />
                 </form>
     <?php
--- pagetemplate-form-page.php
+++ pagetemplate-form-page.php
@@ -18,32 +18,13 @@ get_header(); ?>
 			<?php
 				if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-					if(isset($_POST['message'])) {
-
-						$new_message = $_POST['message'];
-						
-						global $allowedposttags;
-						
-						$allowed_content = array_merge(array(), $allowedposttags);
-						unset($allowed_content['b']);
-
-						$new_message = wp_kses($new_message, $allowed_content);
-						//<b>Bold</b> and <a href="example.com">Länk</a>
-						add_post_meta(get_the_id(), 'secretMessage', $new_message);
-					}
+					var_dump($_POST);
 					
 				}
 
-				$messages = get_post_meta(get_the_id(), 'secretMessage', false);
-				$messages = apply_filters('secret_messages', $messages, get_the_id(), $messages);
 				
-				if(isset($_GET['showMessage']) && $_GET['showMessage'] == '1') {
-					foreach($messages as $message) {
-						echo('<div>'.$message.'</div>');
-					}
-				}
 			?>
-			<?php do_action('before_form_on_page_template', count($messages), $messages); ?>
+			<?php do_action('before_form_on_page_template'); ?>
 			<?php do_action('form_on_page_template'); ?>
 			<?php do_action('after_form_on_page_template'); ?>
         <?php
